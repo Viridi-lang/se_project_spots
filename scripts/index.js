6193c3eb2025df87ab5fe288dfb5a7cd44ebf337
Lina Macias <Lina Macias>
2025-01-22 17:27:07 -0500

    s42ndstage

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,19 @@ const initialCards = [
 ];
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
+const editFormElement = editModal.querySelector(".modal__form")
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#profile-name-input")
+const editModalDescriptionInput = editModal.querySelector("#profile-description-input")
+
 
 function openModal() {
+  editModalNameInput.value = profileName.textContentl;
+  editModalDescriptionInput.value = profileDescription.textContentl;
   editProfileModal.classList.add("modal_opened");
 }
 
@@ -20,6 +28,13 @@ function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
-profileEditButton.addEventListener("click", openModal);
+function handleEditFormSubmit(evt) {
+evt.preventDefault();
+profileName.textContent = editModalNameInput.value;
+profileDescription.textContent = editModalDescriptionInput.value;
+closeModal();
+}
 
-editModalCloseBtn.addEventListener("click", closeModal)
+profileEditButton.addEventListener("click", openModal);
+editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
