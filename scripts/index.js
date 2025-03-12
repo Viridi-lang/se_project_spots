5f86d99d8504a8fb8524de34bc9bac60f3bdf267
Lina Macias <Lina Macias>
2025-03-12 19:40:03 -0400

    projecvt 5 update#2

--- scripts/index.js
+++ scripts/index.js
@@ -30,6 +30,7 @@ const cardLinkInput = cardModal.querySelector("#add-card-link-input")
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
+const previewCloseButton = previewModal.querySelector(".modal__close-btn")
 
 // Card related elements
 const cardTemplate = document.querySelector("#card-template");
@@ -106,6 +107,10 @@ cardModalBtn.addEventListener("click", () => {
 
 cardModalCloseBtn.addEventListener("click", () => {
 closeModal(cardModal);
+});
+
+previewCloseButton.addEventListener("click", () => {
+  closeModal(previewModal);
 });
 
 editFormElement.addEventListener("submit", handleEditFormSubmit);
